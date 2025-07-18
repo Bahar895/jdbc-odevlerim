750af584a6855d35ff359b316ea5a934e51c4a30
Bahar Aydın <bhrs_01@hotmail.com>
2025-07-18 23:17:06 +0300

    Hibernate silme işlemi ödevi eklendi

--- Hibernate_Bebka/src/main/java/org/bebka/jdbc/Main.java
+++ Hibernate_Bebka/src/main/java/org/bebka/jdbc/Main.java
@@ -4,6 +4,7 @@ import org.bebka.jdbc.model.Student;
 import org.bebka.jdbc.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,18 +14,28 @@ public class Main {
 
         // Oturumu başlat
         Session session = factory.openSession();
-        session.beginTransaction();
+        Transaction tx = session.beginTransaction();
 
         // Yeni öğrenci oluştur ve veritabanına kaydet
         Student student = new Student("Bahar", "Aydın");
         session.save(student);
 
         // Veriyi geri okuma
-        Student readStudent = session.get(Student.class, student.getId());
-        System.out.println("Veritabanından okunan öğrenci: " + readStudent);
+        Student readStudent = session.get(Student.class, 4);
+        System.out.println(readStudent);
+
+        //Update
+        readStudent.setName("Derin");
+        readStudent.setSurname("Aydın");
+        session.update(readStudent);
+
+        //Öğrenciyi silme işlemi
+        session.delete(readStudent);
+        System.out.println("Öğrenci silindi" + readStudent);
+
+        tx.commit();
 
         // Oturumu kapat
-        session.getTransaction().commit();
         session.close();
     }
 }
